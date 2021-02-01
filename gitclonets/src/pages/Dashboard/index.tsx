0df1d3b09b3ac756c196acdc89ca37b6d30967dd
Thiago Hartman <th_guto@hotmail.com>
2021-01-31 21:42:56 -0400

    feat: api consumption

---
+++ gitclonets/src/pages/Dashboard/index.tsx
@@ -0,0 +1,65 @@
+import React, { FormEvent, useState } from "react";
+import { FiChevronRight } from "react-icons/fi";
+import logoImg from "../../assets/githeader.svg";
+import { Title, Form, Repositories } from "./styles";
+import api from "../../services/api";
+import { AsyncResource } from "async_hooks";
+
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo("");
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer"></img>
+      <Title>Explore Repositórios no GitHub</Title>
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite aqui"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        ></input>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
+};
+
+export default Dashboard;
---
+++ gitclonets/src/services/api.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+const api = axios.create({
+  baseURL: "http://api.github.com/",
+});
+export default api;
